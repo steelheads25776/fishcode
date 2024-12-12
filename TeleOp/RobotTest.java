72b0918f1c0045d801cd61334facc1d45d7ad07a
steelheads25776 <steelheads25776@gmail.com>
2024-12-11 16:31:42 -0800

    Update RobotTest.java

--- TeleOp/RobotTest.java
+++ TeleOp/RobotTest.java
@@ -4,7 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
 import org.firstinspires.ftc.teamcode.Robot;
 
@@ -13,7 +16,22 @@ public class RobotTest extends LinearOpMode
 {
     GoBildaPinpointDriver odometry;
     DcMotor motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight;
+    DcMotorEx motorSlideLeft, motorSlideRight;
+    DcMotorEx motorClawArm;
+    Servo servoClawGrabber, servoClawExtend;
     Robot bot;
+
+    double orientationCurrent = 0.0; // in degrees - value will be 0 - 359.99
+    double orientationTarget = -1.0;  // -1.0 means no rotation
+    double distanceToTargetOrientation = 0.0;
+    double positionOrientationTarget = 0.0;
+    double positionXTarget = -10000.0;
+    double positionYTarget = -10000.0;
+    double distanceToTargetPosition = 0.0;
+    double[] rotateReturn = new double[2];
+    double[] positionReturn = new double[2];
+    double slideTarget = -1.0;
+    double armTarget = -1.0;
 
     boolean driveButtonA, driveButtonX, driveButtonB, driveButtonY;
     boolean driveDpadL, driveDpadU, driveDpadD, driveDpadR;
@@ -28,8 +46,15 @@ public class RobotTest extends LinearOpMode
     double motorspeedslower = 0.25;
     String rotateMode = "slow";
 
+    double armExtendedPosition = 0.42;//a
+    double armRetractedPosition = 0.0;//x -0.55
+
+    double clawOpenPosition = 0.7;//left bumper
+    double clawClosedPosition = 0.1;//right bumper
+
     public void initialize()
     {
+        // setting hardware to variables
         motorFrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
         motorFrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         motorBackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
@@ -40,11 +65,41 @@ public class RobotTest extends LinearOpMode
         motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        motorSlideLeft = hardwareMap.get(DcMotorEx.class, "SlideLeft");
+        motorSlideRight = hardwareMap.get(DcMotorEx.class, "SlideRight");
+
+        motorSlideLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorSlideRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorSlideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorSlideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorSlideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorSlideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorClawArm = hardwareMap.get(DcMotorEx.class, "Arm");
+
+        motorClawArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorClawArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorClawArm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        servoClawGrabber =  hardwareMap.get(Servo.class, "Grabber");
+        servoClawExtend = hardwareMap.get(Servo.class, "Extender");
+
         odometry = hardwareMap.get(GoBildaPinpointDriver.class, "Odometry");
+        odometry.setOffsets(-124, -150); // don't know how to get these offsets yet
+        odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odometry.resetPosAndIMU();
 
         bot = new Robot(odometry);
-        bot.setDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
-        bot.setMotors(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight);
+        //bot.setDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        //bot.setMotors(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight);
+
+        //bot = new Robot();
+        bot.setMotors(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight, motorSlideLeft, motorSlideRight, motorClawArm);
+        bot.setServos(servoClawGrabber, servoClawExtend);
         bot.setSpeed(0.4);
         bot.reset();
 
@@ -53,7 +108,6 @@ public class RobotTest extends LinearOpMode
 
     public void drive()
     {
-
         stickLeftX = gamepad1.left_stick_x;
         stickLeftY = gamepad1.left_stick_y;
         stickRightX = gamepad1.right_stick_x;
@@ -96,38 +150,98 @@ public class RobotTest extends LinearOpMode
             motorspeed = motorspeednormal;
         }
     }
-
-    public void rotatePad()
+    public void driverDPad()
     {
         driveDpadL = gamepad1.dpad_left;
         driveDpadR = gamepad1.dpad_right;
         driveDpadU = gamepad1.dpad_up;
         driveDpadD = gamepad1.dpad_down;
 
+        /*if(driveDpadL)
+        {
+            orientationTarget = 270;
+        }
+        else if(driveDpadU)
+        {
+            orientationTarget = 0;
+        }
+        else if(driveDpadR)
+        {
+            orientationTarget = 90;
+        }
+        else if(driveDpadD)
+        {
+            orientationTarget = 180;
+        }*/
         if(driveDpadL)
         {
-            bot.rotate(270);
-            bot.sleep(1000);
+            servoClawGrabber.setPosition(clawOpenPosition);
+            positionXTarget = 700;
+            positionYTarget = 0;
+            positionOrientationTarget = 0;
         }
-
-        if(driveDpadU)
+        else if(driveDpadU)
         {
-            bot.rotate(0);
-            bot.sleep(1000);
+            //orientationTarget = 0;
+            //positionXTarget = 0;
+            //positionYTarget = 1000;
+            //positionOrientationTarget = bot.getOrientationCurrent();
+            //slideTarget = 0;
+            armTarget = 2500;
+            servoClawExtend.setPosition(armRetractedPosition);
+            servoClawGrabber.setPosition(clawOpenPosition);
         }
-
-        if(driveDpadR)
+        else if(driveDpadR)
         {
-            bot.rotate(90);
-            bot.sleep(1000);
+            servoClawGrabber.setPosition(clawClosedPosition);
+            orientationTarget = 0;
+            positionXTarget = 0;
+            positionYTarget = 700;
+            positionOrientationTarget = 0;
+            slideTarget = 930;
         }
-
-        if(driveDpadD)
+        else if(driveDpadD)
         {
-            bot.rotate(180);
-            bot.sleep(1000);
+            //orientationTarget = 0;
+            //positionXTarget = 0;
+            //positionYTarget = 0;
+            //positionOrientationTarget = bot.getOrientationCurrent();
+            //slideTarget = 0;
+            armTarget = 1500;
         }
     }
+
+    //public void rotatePad()
+    //{
+    //    driveDpadL = gamepad1.dpad_left;
+    //    driveDpadR = gamepad1.dpad_right;
+    //    driveDpadU = gamepad1.dpad_up;
+    //    driveDpadD = gamepad1.dpad_down;
+
+    //    if(driveDpadL)
+    //    {
+    //        bot.rotate(270);
+    //        bot.sleep(1000);
+    //    }
+
+    //    if(driveDpadU)
+    //    {
+    //        bot.rotate(0);
+    //        bot.sleep(1000);
+    //    }
+
+    //    if(driveDpadR)
+    //    {
+    //        bot.rotate(90);
+    //        bot.sleep(1000);
+    //    }
+
+    //    if(driveDpadD)
+    //    {
+    //        bot.rotate(180);
+    //        bot.sleep(1000);
+    //    }
+    //}
 
     public void resetRobot()
     {
@@ -146,7 +260,7 @@ public class RobotTest extends LinearOpMode
 
         if(driveButtonY)
         {
-            bot.forward(100);
+            bot.forward(1000);
             bot.sleep(1000);
         }
     }
@@ -186,28 +300,54 @@ public class RobotTest extends LinearOpMode
         bot.update();
         telemetry.addData("current x", bot.getX());
         telemetry.addData("current y", bot.getY());
-        telemetry.addData("current target", bot.getTargetPos());
-        telemetry.addData("current degrees", bot.getDegrees());
-        telemetry.addData("current radians", bot.getRad());
-        telemetry.addData("distance to target", bot.targetDistance);
-        telemetry.addData("spin direction", bot.spinDirection);
+        telemetry.addData("current target", orientationTarget);
+        telemetry.addData("target slide", slideTarget);
+        telemetry.addData("current slide", motorSlideLeft.getCurrentPosition());
+        telemetry.addData("test", bot.test);
+
+        telemetry.addData("current orientation", bot.getOrientationCurrent());
+        //telemetry.addData("current radians", bot.getRad());
+        telemetry.addData("distance to target rotation", distanceToTargetOrientation);
+        telemetry.addData("distance to target position", distanceToTargetPosition);
+        telemetry.addData("stopped position", bot.YStoppedPosition);
+        telemetry.addData("armSlide target", slideTarget);
+        telemetry.addData("slide position", motorSlideLeft.getCurrentPosition());
+        telemetry.addData("clawArm target", armTarget);
+        telemetry.addData("clawArm position", motorClawArm.getCurrentPosition());
+        telemetry.addData("arm position", bot.test);
         telemetry.update();
     }
-
 
     public void runOpMode()
     {
         initialize();
         while(opModeIsActive())
         {
+            drive();
+            rotateReturn = bot.navRotate(orientationTarget);
+            distanceToTargetOrientation = rotateReturn[0];
+            orientationTarget = rotateReturn[1];
+
+            if(orientationTarget < 0)
+            {
+                positionReturn = bot.navToPosition(positionXTarget, positionYTarget, positionOrientationTarget);
+                distanceToTargetPosition = positionReturn[0];
+                positionXTarget = positionReturn[1];
+            }
+
+            slideTarget = bot.slideToPosition(slideTarget);
+            armTarget = bot.armToPosition(armTarget);
+
+            driverDPad();
+            showTelemetry();
+
             forward();
             backward();
-            drive();
             left();
             right();
-            rotatePad();
+
+            //rotatePad();
             //resetRobot();
-            showTelemetry();
         }
     }
 }
