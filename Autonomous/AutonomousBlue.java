4b0c96c50fb22a4418b8cd2252558827d75147c6
steelheads25776 <steelheads25776@gmail.com>
2024-12-11 16:30:45 -0800

    Update AutonomousBlue.java

--- Autonomous/AutonomousBlue.java
+++ Autonomous/AutonomousBlue.java
@@ -3,53 +3,197 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
 import org.firstinspires.ftc.teamcode.Robot;
 
+import java.nio.file.FileAlreadyExistsException;
+
 @Autonomous
 public class AutonomousBlue extends OpMode
 {
-    DcMotor frontLeft, frontRight, backLeft, backRight;
     GoBildaPinpointDriver odometry;
+    DcMotor motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight;
+    DcMotorEx motorSlideLeft, motorSlideRight;
+    DcMotorEx motorClawArm;
+    Servo servoClawGrabber, servoClawExtend;
     Robot bot;
+
+    double orientationCurrent = 0.0; // in degrees - value will be 0 - 359.99
+    double orientationTarget = -1.0;  // -1.0 means no rotation
+    double distanceToTargetOrientation = 0.0;
+    double positionOrientationTarget = 0.0;
+    double positionXTarget = -10000.0;
+    double positionYTarget = -10000.0;
+    double distanceToTargetPosition = 0.0;
+    double[] rotateReturn = new double[2];
+    double[] positionReturn = new double[2];
+    double slideTarget = -1.0;
+    double armTarget = -1.0;
+
+    boolean driveButtonA, driveButtonX, driveButtonB, driveButtonY;
+    boolean driveDpadL, driveDpadU, driveDpadD, driveDpadR;
+    float stickLeftX, stickLeftY, stickRightX, stickRightY;
+    float triggerLeft, triggerRight;
+
+    double motorspeed = 1.0;
+    double rotateSpeed = 0.75;
+    double slowRotateSpeed = 0.35;
+    double motorspeedhigh = 1.0;
+    double motorspeednormal = 0.5;
+    double motorspeedslower = 0.25;
+    String rotateMode = "slow";
+
+    double armExtendedPosition = 0.42;//a
+    double armRetractedPosition = 0.0;//x -0.55
+
+    double clawOpenPosition = 0.7;//left bumper
+    double clawClosedPosition = 0.1;//right bumper
+
+    boolean step1, step2, step3, step4, step5 = false;
+    boolean step1Started, step2Started, step3Started, step4Started, step5Started = false;
 
     public void showTelemetry()
     {
-        telemetry.addData("degrees", bot.getDeg());
+        //telemetry.addData("degrees", bot.getDeg());
+        telemetry.addLine("loop");
+        telemetry.addData("x target", positionXTarget);
+        telemetry.addData("x pos", positionReturn[0]);
+        telemetry.addData("y pos", positionReturn[1]);
+        telemetry.addData("orientation target", orientationTarget);
+        telemetry.addData("orientation distance to target", rotateReturn[0]);
+        telemetry.addData("arm target", armTarget);
+        telemetry.addData("step1", step1);
+        telemetry.addData("step2", step2);
+        telemetry.addData("step3", step3);
+        telemetry.addData("step4", step4);
+        telemetry.addData("step5", step5);
+
         bot.update();
         telemetry.update();
     }
 
     public void init()
     {
-        frontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
-        backRight = hardwareMap.get(DcMotor.class, "BackRight");
+        // setting hardware to variables
+        motorFrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        motorFrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        motorBackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        motorBackRight = hardwareMap.get(DcMotor.class, "BackRight");
 
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorSlideLeft = hardwareMap.get(DcMotorEx.class, "SlideLeft");
+        motorSlideRight = hardwareMap.get(DcMotorEx.class, "SlideRight");
+
+        motorSlideLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorSlideRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorSlideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorSlideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorSlideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorSlideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorClawArm = hardwareMap.get(DcMotorEx.class, "Arm");
+
+        motorClawArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorClawArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorClawArm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        servoClawGrabber =  hardwareMap.get(Servo.class, "Grabber");
+        servoClawExtend = hardwareMap.get(Servo.class, "Extender");
 
         odometry = hardwareMap.get(GoBildaPinpointDriver.class, "Odometry");
+        odometry.setOffsets(-124, -150); // don't know how to get these offsets yet
+        odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odometry.resetPosAndIMU();
+
         bot = new Robot(odometry);
-        bot.setDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        //bot.setDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        //bot.setMotors(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight);
+
+        //bot = new Robot();
+        bot.setMotors(motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight, motorSlideLeft, motorSlideRight, motorClawArm);
+        bot.setServos(servoClawGrabber, servoClawExtend);
+        bot.setSpeed(0.4);
         bot.reset();
-        bot.setMotors(frontLeft, frontRight, backLeft, backRight);
-        //bot.setOffsets(0, 0); // i do not know these values
     }
 
     public void start()
     {
-        bot.rotate(-90);
+        telemetry.addLine("start");
+        telemetry.update();
+        /*
+        positionXTarget = 0;
+        positionYTarget = 1000;
+        while(positionXTarget > -10000);
+
+        orientationTarget = 180;
+        while(orientationTarget > -1);
+
+        armTarget = 2500;
+        while(armTarget > -1);
+
+        armTarget = 0;
+        while(armTarget > -1);
+
+        orientationTarget = 0;
+        while(orientationTarget > -1);
+
+        positionXTarget = 0;
+        positionYTarget = 0;
+
+         */
     }
 
     public void loop()
     {
         showTelemetry();
+
+        rotateReturn = bot.navRotate(orientationTarget);
+        distanceToTargetOrientation = rotateReturn[0];
+        orientationTarget = rotateReturn[1];
+
+        if(orientationTarget < 0)
+        {
+            positionReturn = bot.navToPosition(positionXTarget, positionYTarget, positionOrientationTarget);
+            distanceToTargetPosition = positionReturn[0];
+            positionXTarget = positionReturn[1];
+        }
+
+        slideTarget = bot.slideToPosition(slideTarget);
+        armTarget = bot.armToPosition(armTarget);
+
+        if(step1 == false && step1Started == false)
+        {
+            positionXTarget = 0;
+            positionYTarget = 1000;
+            step1Started = true;
+        }
+        else if(step1 == false && positionXTarget < -9999)
+        {
+            step1 = true;
+        }
+
+        if(step2 == false && step1 == true && step2Started == false)
+        {
+            orientationTarget = 180;
+            step2Started = true;
+        }
+        else if(step2 == false && step1 == true && orientationTarget < 0)
+        {
+            step2 = true;
+        }
+
     }
 }
